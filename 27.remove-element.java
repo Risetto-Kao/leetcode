7d45711b398385d492446da585dba2062582be58
B07207012 <b07207012@ntu.edu.tw>
2024-09-25 22:09:16 +0800

    complete

---
+++ 27.remove-element.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=27 lang=java
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int l = 0;
+        int r = nums.length;
+        while (l < r) {
+            if (nums[l] == val) {
+                nums[l] = nums[r - 1];
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return r;
+    }
+}
+
+// @lc code=end
--- README.md
+++ README.md
@@ -4,3 +4,9 @@
   - snippets:
   - shortcut:
 - java
+
+# Problem List
+- https://leetcode.com/studyplan/top-interview-150/
+
+# Done List
+- https://www.notion.so/ff35a3448cdf4432b08a1151aaa24e5e?v=91e942f68f5b40d28743629b21274864&pvs=4
